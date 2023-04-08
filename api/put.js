0d544cdd10c5d3a6db76fc4773652b7221f4e97f
Harsh <harsh.kumar@dhwaniris.com>
2023-04-08 14:47:56 +0530

    minor

--- api/put.js
+++ api/put.js
@@ -1,9 +1,11 @@
-module.exports.update = async function (req, res) {
+module.exports.update = async function (req, res, next) {
     try {
         let condition = { isActive: true, _id: req.params._id };
-        let data = await req.crudModel.findOneAndUpdate(condition, { $set: req.body }, { new: true });
 
-        if (data) {
+        let dataToUpdate = req.body;
+
+        let data = await req.crudModel.findOneAndUpdate(condition, { $set: dataToUpdate }, { new: true });
+        if (!data) {
             return res.badRequest({}, `Record not found: ${req.params._id}`);
         }
 
