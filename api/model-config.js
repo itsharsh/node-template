c521826a5b5a1f0b9cebd1bd6b06194c5eeb0cf8
Harsh <harsh.kumar@dhwaniris.com>
2023-04-17 15:13:08 +0530

    model config data changes

--- api/model-config.js
+++ api/model-config.js
@@ -1,8 +1,10 @@
 module.exports = (req, res) => {
     let modelConfig = JSON.parse(JSON.stringify(req.crudModel.schema.obj));
+    let dataToSend = [];
     Object.keys(modelConfig).map((k) => {
         delete modelConfig[k].type;
-        !modelConfig[k]?.config?.inputType && delete modelConfig[k];
+        dataToSend.push({ key: k, ...modelConfig[k] });
+        !modelConfig[k]?.config?.inputType && delete modelConfig[k] && dataToSend.pop();
     });
-    return res.ok(modelConfig, `Successfully fetched model config for ${req.crudModel.modelName}`);
+    return res.ok(dataToSend, `Successfully fetched model config for ${req.crudModel.modelName}`);
 };
