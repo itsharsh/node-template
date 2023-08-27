910556f6226bfff7b096c81d71cdcbe1f0c96737
Harsh <harsh.kumar@dhwaniris.com>
2023-08-27 22:32:29 +0530

    minor

--- api/index.js
+++ api/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 const basicCrud = Object.keys(require("../../../models"));
-basicCrud.map((e) => require(`../../../models`)[e]);
+basicCrud.forEach((e) => require(`../../../models`)[e]);
 
 const initModel = (modelName) => (req, res, next) =>
     (req.crudModelName = modelName) && (req.crudModel = require(`../../../models`)[modelName]) && next();
-basicCrud.map((model) => app.use(`/${model.toLowerCase()}`, initModel(model), require("./route")));
+basicCrud.forEach((model) => app.use(`/${model.toLowerCase()}`, initModel(model), require("./route")));
 
 module.exports = app;
--- middlewares/response/index.js
+++ middlewares/response/index.js
@@ -1,9 +1,9 @@
 module.exports = (req, res, next) => {
-    res["ok"] = require("./ok");
-    res["dBError"] = require("./dbError");
-    res["badRequest"] = require("./badRequest");
-    res["internalError"] = require("./internalError");
-    res["unAuthorized"] = require("./unAuthorized");
-    res["notFound"] = require("./notFound");
+    res.ok = require("./ok");
+    res.dBError = require("./dbError");
+    res.badRequest = require("./badRequest");
+    res.internalError = require("./internalError");
+    res.unAuthorized = require("./unAuthorized");
+    res.notFound = require("./notFound");
     next();
 };
