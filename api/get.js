dc53d03abd5629147d20b04b6d852dad47363e03
Harsh Kumar Singh <hkumarsingh142@gmail.com>
2023-09-03 03:15:39 +0530

    filters and populate data from req.body

--- api/get.js
+++ api/get.js
@@ -1,7 +1,9 @@
-module.exports.getAll = async function (req, res) {
+exports.getAll = async function (req, res) {
     try {
-        let condition = { isActive: true };
-        const data = await req.crudModel.find(condition).populate(getReferencedSchema(req)).lean();
+        const model = req.crudModel;
+        const filters = getFilters(req.body);
+        const populateData = getReferencedSchema(req.body, model);
+        const data = await req.crudModel.find(filters).populate(populateData).lean();
         return res.ok(
             { data, total: data.length },
             `Successfully fetched all ${req.crudModel.collection.collectionName.slice(0, -1)}(s)`,
@@ -11,11 +13,11 @@ module.exports.getAll = async function (req, res) {
     }
 };
 
-module.exports.getById = async function (req, res) {
+exports.getById = async function (req, res) {
     try {
-        let condition = { isActive: true };
-        Object.assign(condition, { _id: req.params._id });
-        const data = await req.crudModel.findOne(condition).populate(getReferencedSchema(req)).lean();
+        const filters = { isActive: true };
+        Object.assign(filters, { _id: req.params._id });
+        const data = await req.crudModel.findOne(filters).populate(getReferencedSchema(req)).lean();
         if (!data) {
             return res.status(400).json({ success: false, message: "Data not found", total: 0, data: [] });
         }
@@ -25,19 +27,26 @@ module.exports.getById = async function (req, res) {
     }
 };
 
-function getReferencedSchema(req) {
-    let referencedSchema = "";
-    const model = req.crudModel;
-    if (req?.query?.populate == "true" || req?.query?.populateKeys) {
-        const populateKeys = req.query?.populateKeys?.split(",");
-        referencedSchema = Object.keys(model.schema.obj)
-            .map((e) =>
-                Array.isArray(model.schema.obj[e])
-                    ? model.schema.obj[e][0].hasOwnProperty("ref") && e
-                    : model.schema.obj[e].hasOwnProperty("ref") && e,
-            )
-            .filter((e) => (populateKeys?.length ? populateKeys.includes(e) : e))
-            .join(" ");
+function getFilters(body) {
+    const filters = { isActive: true };
+    body?.filters && Object.assign(filters, body.filters);
+    return filters;
+}
+
+function getReferencedSchema(body, model) {
+    const referencedSchema = [];
+    if (body?.populate == "true" || body?.populateKeys) {
+        Object.keys(model.schema.obj).forEach((e) => {
+            const referencedKey = Array.isArray(model.schema.obj[e])
+                ? model.schema.obj[e][0].hasOwnProperty("ref") && model.schema.obj[e][0]
+                : model.schema.obj[e].hasOwnProperty("ref") && model.schema.obj[e];
+            referencedKey &&
+                referencedSchema.push({
+                    path: e,
+                    model: model.schema.obj[e].ref,
+                    select: body?.populateSelect[e] || "",
+                });
+        });
     }
     return referencedSchema;
 }
