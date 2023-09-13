85af54df3bfa52045d423a60aab3c9e7f02ab71d
Harsh Kumar Singh <hkumarsingh142@gmail.com>
2023-09-13 15:03:38 +0530

    key in model config

--- api/model-config.js
+++ api/model-config.js
@@ -1,9 +1,10 @@
 module.exports = (req, res) => {
     let modelConfig = JSON.parse(JSON.stringify(req.crudModel.schema.obj));
     let dataToSend = [];
-    Object.keys(modelConfig).map((k) => {
+    Object.keys(modelConfig).forEach((k) => {
         delete modelConfig[k].type;
-        dataToSend.push({ key: k, ...modelConfig[k] });
+        modelConfig[k].config && (modelConfig[k].config.key = k);
+        dataToSend.push(modelConfig[k]);
         !modelConfig[k]?.config?.inputType && delete modelConfig[k] && dataToSend.pop();
     });
     return res.ok(dataToSend, `Successfully fetched model config for ${req.crudModel.modelName}`);
