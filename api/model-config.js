303b49934fb271e1d97bf93d216e88dedfcdb30d
Harsh Kumar Singh <hkumarsingh142@gmail.com>
2023-09-30 00:39:01 +0530

    minor

--- api/model-config.js
+++ api/model-config.js
@@ -4,6 +4,8 @@ module.exports = (req, res, next) => {
         let data = [];
         Object.keys(modelConfig).forEach((k) => {
             delete modelConfig[k].type;
+            delete modelConfig[k].index;
+            delete modelConfig[k].enum;
             modelConfig[k].config && (modelConfig[k].config.key = k);
             data.push(modelConfig[k]);
             !modelConfig[k]?.config?.inputType && delete modelConfig[k] && data.pop();
